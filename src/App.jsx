12d128c34dea0784420b06f68d0b86dcb3ac25b7
muhirethierry <muhiremthierry@gmail.com>
2026-07-14 19:49:23 +0200

    Fix component import filename casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
-import Services from "./components/Services/Services";
-import WhyChoose from "./components/WhyChoose/WhyChoose";
+import Services from "./components/Services/services";
+import WhyChoose from "./components/WhyChoose/whychoose";
 import Statistics from "./components/Statistics/Statistics";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import WhatsApp from "./components/Whatsapp/Whatsapp";
+import WhatsApp from "./components/Whatsapp/whatsapp";
 import ScrollToTop from "./components/scrolltotop/scrolltotop";
 function App() {
   return (
